4c5d228b5acc163e6d3b5e6eece3ec23f7d0098f
KimDoHyun <kdhqwe1030@naver.com>
2024-05-23 22:28:28 +0900

    Feat Character UI implementation and character movement animation

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ canvas.height = 576;
 
 c.fillRect(0, 0, canvas.width, canvas.height);
 const gravity = 0.73;
-
+//배경
 const background = new Sprite({
   position: {
     x: 0,
@@ -15,6 +15,18 @@ const background = new Sprite({
   },
   imageSrc: './img/background.png',
 });
+
+//상점
+const shop = new Sprite({
+  position: {
+    x: 600,
+    y: 128,
+  },
+  imageSrc: './img/shop.png',
+  scale: 2.75,
+  framesMax: 6,
+});
+
 //플레이어 1, 2 선언
 const player = new Fighter({
   position: {
@@ -28,6 +40,51 @@ const player = new Fighter({
   offset: {
     x: 0,
     y: 0,
+  },
+  imageSrc: './img/samuraiMack/Idle.png',
+  framesMax: 8,
+  scale: 2.5,
+  offset: {
+    x: 215,
+    y: 157,
+  },
+  sprites: {
+    idle: {
+      imageSrc: './img/samuraiMack/Idle.png',
+      framesMax: 8,
+    },
+    run: {
+      imageSrc: './img/samuraiMack/Run.png',
+      framesMax: 8,
+    },
+    jump: {
+      imageSrc: './img/samuraiMack/Jump.png',
+      framesMax: 2,
+    },
+    fall: {
+      imageSrc: './img/samuraiMack/Fall.png',
+      framesMax: 2,
+    },
+    attack1: {
+      imageSrc: './img/samuraiMack/Attack1.png',
+      framesMax: 6,
+    },
+    takeHit: {
+      imageSrc: './img/samuraiMack/Take Hit - white silhouette.png',
+      framesMax: 4,
+    },
+    death: {
+      imageSrc: './img/samuraiMack/Death.png',
+      framesMax: 6,
+    },
+  },
+  attackBox: {
+    offset: {
+      x: 100,
+      y: 50,
+    },
+    width: 170,
+    height: 50,
   },
 });
 
@@ -46,6 +103,51 @@ const enemy = new Fighter({
   offset: {
     x: -50,
     y: 0,
+  },
+  imageSrc: './img/kenji/Idle.png',
+  framesMax: 4,
+  scale: 2.5,
+  offset: {
+    x: 215,
+    y: 167,
+  },
+  sprites: {
+    idle: {
+      imageSrc: './img/kenji/Idle.png',
+      framesMax: 4,
+    },
+    run: {
+      imageSrc: './img/kenji/Run.png',
+      framesMax: 8,
+    },
+    jump: {
+      imageSrc: './img/kenji/Jump.png',
+      framesMax: 2,
+    },
+    fall: {
+      imageSrc: './img/kenji/Fall.png',
+      framesMax: 2,
+    },
+    attack1: {
+      imageSrc: './img/kenji/Attack1.png',
+      framesMax: 4,
+    },
+    takeHit: {
+      imageSrc: './img/kenji/Take hit.png',
+      framesMax: 3,
+    },
+    death: {
+      imageSrc: './img/kenji/Death.png',
+      framesMax: 7,
+    },
+  },
+  attackBox: {
+    offset: {
+      x: -175,
+      y: 50,
+    },
+    width: 170,
+    height: 50,
   },
 });
 enemy.draw();
@@ -69,36 +171,65 @@ function animate() {
   c.fillStyle = 'black';
   c.fillRect(0, 0, canvas.width, canvas.height);
   background.update();
+  shop.update();
   player.update();
   enemy.update();
 
-  //플레이어와 적 움직이기
+  //플레이어와 플레이어2 움직이기
   player.velocity.x = 0;
   enemy.velocity.x = 0;
+
+  //플레이어 움직이기
+
   if (keys.a.pressed && lastKey === 'a') {
     player.velocity.x = -5;
+    player.switchSprite('run');
   } else if (keys.d.pressed && lastKey === 'd') {
     player.velocity.x = 5;
+    player.switchSprite('run');
+  } else {
+    player.switchSprite('idle');
   }
 
+  if (player.velocity.y < 0) {
+    player.switchSprite('jump');
+  } else if (player.velocity.y > 0) {
+    player.switchSprite('fall');
+  }
+
+  //플레이어2움직이기
   if (keys.ArrowLeft.pressed && enemy.lastKey === 'ArrowLeft') {
     enemy.velocity.x = -5;
+    enemy.switchSprite('run');
   } else if (keys.ArrowRight.pressed && enemy.lastKey === 'ArrowRight') {
     enemy.velocity.x = 5;
+    enemy.switchSprite('run');
+  } else {
+    enemy.switchSprite('idle');
+  }
+  if (enemy.velocity.y < 0) {
+    enemy.switchSprite('jump');
+  } else if (enemy.velocity.y > 0) {
+    enemy.switchSprite('fall');
   }
 
-  //공격 감지
+  //공격 감지 &맞는 모션
   if (
     rectangularCollision({
       rectangel1: player,
       rectangel2: enemy,
     }) &&
-    player.isAttacking
+    player.isAttacking &&
+    player.frameCurrent === 4
   ) {
+    enemy.takeHit();
     player.isAttacking = false;
-    enemy.health -= 20;
-    er;
+
     document.querySelector('#enemyHealth').style.width = enemy.health + '%';
+  }
+
+  if (player.isAttacking && player.frameCurrent === 4) {
+    player.isAttacking = false;
   }
 
   if (
@@ -106,11 +237,16 @@ function animate() {
       rectangel1: enemy,
       rectangel2: player,
     }) &&
-    enemy.isAttacking
+    enemy.isAttacking &&
+    enemy.frameCurrent === 2
   ) {
+    player.takeHit();
     enemy.isAttacking = false;
-    player.health -= 20;
     document.querySelector('#playerHealth').style.width = player.health + '%';
+  }
+
+  if (enemy.isAttacking && enemy.frameCurrent === 2) {
+    enemy.isAttacking = false;
   }
   //체력을 다 잃는다면
   if (enemy.health <= 0 || player.health <= 0) {
@@ -121,36 +257,41 @@ animate();
 
 //키 설정
 window.addEventListener('keydown', (event) => {
-  switch (event.key) {
-    case 'd':
-      keys.d.pressed = true;
-      lastKey = 'd';
-      break;
-    case 'a':
-      keys.a.pressed = true;
-      lastKey = 'a';
-      break;
-    case 'w':
-      player.velocity.y = -20;
-      break;
-    case ' ':
-      player.attack();
-      break;
-
-    case 'ArrowRight':
-      keys.ArrowRight.pressed = true;
-      enemy.lastKey = 'ArrowRight';
-      break;
-    case 'ArrowLeft':
-      keys.ArrowLeft.pressed = true;
-      enemy.lastKey = 'ArrowLeft';
-      break;
-    case 'ArrowUp':
-      enemy.velocity.y = -20;
-      break;
-    case 'ArrowDown':
-      enemy.isAttacking = true;
-      break;
+  if (!player.dead) {
+    switch (event.key) {
+      case 'd':
+        keys.d.pressed = true;
+        lastKey = 'd';
+        break;
+      case 'a':
+        keys.a.pressed = true;
+        lastKey = 'a';
+        break;
+      case 'w':
+        player.velocity.y = -20;
+        break;
+      case ' ':
+        player.attack();
+        break;
+    }
+  }
+  if (!enemy.dead) {
+    switch (event.key) {
+      case 'ArrowRight':
+        keys.ArrowRight.pressed = true;
+        enemy.lastKey = 'ArrowRight';
+        break;
+      case 'ArrowLeft':
+        keys.ArrowLeft.pressed = true;
+        enemy.lastKey = 'ArrowLeft';
+        break;
+      case 'ArrowUp':
+        enemy.velocity.y = -20;
+        break;
+      case 'ArrowDown':
+        enemy.attack();
+        break;
+    }
   }
 });
 window.addEventListener('keyup', (event) => {
