01599cfc024e864b14fecb2fd506da667d6db459
KimDoHyun <kdhqwe1030@naver.com>
2024-05-23 23:26:12 +0900

    Release v1.0.0

--- index.js
+++ index.js
@@ -172,6 +172,8 @@ function animate() {
   c.fillRect(0, 0, canvas.width, canvas.height);
   background.update();
   shop.update();
+  c.fillStyle = 'rgba(255,255,255,0.15)';
+  c.fillRect(0, 0, canvas.width, canvas.height);
   player.update();
   enemy.update();
 
@@ -225,7 +227,9 @@ function animate() {
     enemy.takeHit();
     player.isAttacking = false;
 
-    document.querySelector('#enemyHealth').style.width = enemy.health + '%';
+    gsap.to('#enemyHealth', {
+      width: enemy.health + '%',
+    });
   }
 
   if (player.isAttacking && player.frameCurrent === 4) {
@@ -242,7 +246,9 @@ function animate() {
   ) {
     player.takeHit();
     enemy.isAttacking = false;
-    document.querySelector('#playerHealth').style.width = player.health + '%';
+    gsap.to('#playerHealth', {
+      width: player.health + '%',
+    });
   }
 
   if (enemy.isAttacking && enemy.frameCurrent === 2) {
